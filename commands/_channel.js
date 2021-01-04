cf2876743e7c83d3974c956ed4c93c0efe968f75
Bots.Business <git.sync@bots.business>
2021-01-04 09:55:38 +0000

    Bots.Business export for bot id: 170563

---
+++ commands/_channel.js
@@ -0,0 +1,12 @@
+/*CMD
+  command: /channel
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: Channel : https://t.me/bot_movies1
+  keyboard: 
+  aliases: 
+CMD*/
+
+https://t.me/bot_movies1
---
+++ commands/_start.js
@@ -0,0 +1,23 @@
+/*CMD
+  command: /start
+  help: 
+  need_reply: true
+  auto_retry_time: 1
+  folder: 
+
+  <<ANSWER
+Hello ! 
+
+🎭 Join Below Channel And Group 🎭
+
+(ask your request movies or any doubt in below Group or channel)
+  ANSWER
+  keyboard: 
+  aliases: hello !   🎭 join below channel and group 🎭  (ask your request movies or any doubt in below group or channel)
+CMD*/
+
+// Hello ! 
+
+🎭 Join Below Channel And Group 🎭
+
+(ask your request movies or any doubt in below Group or channel)
